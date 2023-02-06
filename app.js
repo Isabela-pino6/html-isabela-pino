0169d8e6facb1c6396637c3b431cad5fcca4486e
Isabela-pino6 <123569893+Isabela-pino6@users.noreply.github.com>
2023-02-06 12:05:29 -0500

    crear clase

---
+++ app.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded"), () => { 
+  const cardsAdj =  [
+    {
+      name: 'descarga',
+      img:'img/descarga.jpg'
+     }
+
+     {
+      name: 'fe',
+      img:'img/fe.jpg'
+     }
+
+     {
+      name: 'feid',
+      img:'img/feid.jpg'
+     }
+
+     {
+      name: 'feidd',
+      img:'img/feidd.jpg'
+     }
+
+     {
+      name: 'fe',
+      img:'img/fe.jpg'
+     }
+
+     {
+      name: 'fe',
+      img:'img/fe.jpg'
+     }
+
+     {
+      name: 'fe',
+      img:'img/fe.jpg'
+     }
+
+     {
+      name: 'fe',
+      img:'img/fe.jpg'
+     }
+
+  ] 
+
+});
