5720f957a41a5d90f63a0f6b93babb7e44cf00ae
Isabela-pino6 <123569893+Isabela-pino6@users.noreply.github.com>
2023-03-13 12:01:04 -0500

    revisión

--- app.js
+++ app.js
@@ -1,65 +1,81 @@
-document.addEventListener("DOMContentLoaded"), () => { 
-  const cardsAdj =  [
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
     {
-      name: 'descarga',
-      img:'imagenes/descarga.jpg'
+      name: "descarga",
+      img: "imagenes/descarga.jpg"
     },
-     {
-      name: 'fe',
-      img:'imagenes/fe.jpg'
-     },
-     {
-      name: 'feid',
-      img:'imagenes/feid.jpg'
-     },
-     {
-      name: 'feidd',
-      img:'imagenes/feidd.jpg'
-     },
-     {
-      name: 'feiid',
-      img:'imagenes/feiid.jpg'
-     },
-     {
-      name: 'ferxxoo',
-      img:'imagenes/ferxxoo.jpg'
-     },
-     {
-      name: 'imagen',
-      img:'imagenes/imagen.jpg'
-     },
-     {
-      name: 'salomon',
-      img:'imagenes/salomon.jpg'
-     },
+    {
+      name: "fe",
+      img: "imagenes/fe.jpg"
+    },
+    {
+      name: "feid",
+      img: "imagenes/feid.jpg"
+    },
+    {
+      name: "feidd",
+      img: "imagenes/feidd.jpg"
+    },
+    {
+      name: "feiid",
+      img: "imagenes/feiid.jpg"
+    },
+    {
+      name: "ferxxoo",
+      img: "imagenes/ferxxoo.jpg"
+    },
+    {
+      name: "descarga",
+      img: "imagenes/descarga.jpg"
+    },
+    {
+      name: "fe",
+      img: "imagenes/fe.jpg"
+    },
+    {
+      name: "feid",
+      img: "imagenes/feid.jpg"
+    },
+    {
+      name: "feidd",
+      img: "imagenes/feidd.jpg"
+    },
+    {
+      name: "feiid",
+      img: "imagenes/feiid.jpg"
+    },
+    {
+      name: "ferxxoo",
+      img: "imagenes/ferxxoo.jpg"
+    }
   ];
-});
 
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasEscogidas = [];
-var cartasEscogidasId = [];
-var cartasGanadas = [];
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
 
-function crearTablero (){
-  for (let i = 0; i < cardAdj.length; i++) {
+  function crearTablero() {
+    for (let i = 0; i < cardAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "imagenes/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
+  }
 
-    var carta = document.createElement('img');
-    carta.setAttribute('src', 'imagenes/reverso.png');
-
-    carta.setAttribute('data-id', i);
-    carta.addEventListenner('click', voltearCarta);
-
-    cuadricula.appendChild(carta);
-
-    function voltearCarta() {
-      var cardId = this.getAttribute("data-id");
-      cartasEscogidas.push(cardAdj[cardId].name);
-      cartasEscogidasId.push(cardId);
-      if (cartasEscogidas.length === 2) {
-        setTimeout(verificarPareja, 1000);
-      }
-    } 
+  //---------------------------lecture04--------------------------//
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1500);
+    }
+  }
 
   crearTablero();
 });
